2850fd5360cd948d039808cea258fdc6e46fea70
RobertoRuben <robertoch263@gmail.com>
2025-01-15 18:39:09 -0500

    ✨ feat(repository): agregar repositorio PromoterRepository

---
+++ src/main/java/com/porseacaso/qrpasshubapi/repository/PromoterRepository.java
@@ -0,0 +1,7 @@
+package com.porseacaso.qrpasshubapi.repository;
+
+import com.porseacaso.qrpasshubapi.model.entity.Promoter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PromoterRepository extends JpaRepository<Promoter, Integer> {
+}
