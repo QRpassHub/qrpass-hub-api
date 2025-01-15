21ff865e12c4f96936a59661811bc392a6a0b5a6
chave <chave@CHVEZ05>
2025-01-15 13:59:09 -0500

    🎉 init: estructura inicial del proyecto

---
+++ src/main/java/com/porseacaso/qrpasshubapi/QrpassHubApiApplication.java
@@ -0,0 +1,13 @@
+package com.porseacaso.qrpasshubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrpassHubApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrpassHubApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/porseacaso/qrpasshubapi/QrpassHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porseacaso.qrpasshubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrpassHubApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
