0b1924ab9bafcbebbc269706a003ab2cc3146dd6
RobertoRuben <robertoch263@gmail.com>
2025-01-23 21:04:25 -0500

    ✨ feat(entity): implementar la entidad Ticket y establecer las asociaciones con las clases Cludgoer y Promoter

---
+++ src/main/java/com/porseacaso/qrpasshubapi/model/entity/Ticket.java
@@ -0,0 +1,44 @@
+package com.porseacaso.qrpasshubapi.model.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "tickets")
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "uuid", nullable = false, unique = true, columnDefinition = "Text")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private Float uuid;
+
+    @Column(name = "creation_date", nullable = false, columnDefinition = "TIMESTAMP")
+    private LocalDateTime creationDate;
+
+    @Column(name = "expiration_date", nullable = true, columnDefinition = "TIMESTAMP")
+    private LocalDateTime expirationDate;
+
+    @Column(name = "is_confirmed", nullable = false, columnDefinition = "BOOLEAN")
+    private Boolean isConfirmed;
+
+
+    @ManyToOne
+    @JoinColumn(name = "cludgoer_id", referencedColumnName = "id",
+            foreignKey = @ForeignKey(name = "FK_ticket_cludgoer"))
+    private Cludgoer cludgoer;
+
+    @ManyToOne
+    @JoinColumn(name = "promoter_id", referencedColumnName = "id",
+            foreignKey = @ForeignKey(name = "FK_ticket_promoter"))
+    private Promoter promoter;
+
+
+
+}
